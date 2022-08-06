b19e01cfa44d333e5d666ed0bd91b1fa1aa9baed
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-05 20:15:40 -0400

    TCP streams

--- src/main/scala/io/github/edadma/spritz/Server.scala
+++ src/main/scala/io/github/edadma/spritz/Server.scala
@@ -2,7 +2,14 @@ package io.github.edadma.spritz
 
 import pprint.pprintln
 
-abstract class Server(val serverName: String):
+import scala.scalanative.libc.stdlib.malloc
+import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
+
+abstract class Server(address: String, port: Int, flags: Int, backlog: Int, val serverName: String):
+  import io.github.edadma.spritz.libuv._
+  import io.github.edadma.spritz.libuvConstants._
+
   def main: Router => Unit
 
   protected val router = new Router
@@ -16,13 +23,35 @@ abstract class Server(val serverName: String):
 
   /////////////
 
-  println("listening")
+  val SOCKADDR_IN = 16
+  val socketAddress: Ptr[Byte] = stackalloc[Byte](SOCKADDR_IN.toUInt)
+
+  Zone { implicit z =>
+    checkError(uv_ip4_addr(CString.fromString(address), port, socketAddress))
+  }
+
+  val server: TCPHandle = malloc(uv_handle_size(UV_TCP_T)).asInstanceOf[TCPHandle]
+  val loop: Loop = uv_default_loop()
+
+  checkError(uv_tcp_init(loop, server), "uv_tcp_init(server)")
+  checkError(uv_tcp_bind(server, socketAddress, flags), "uv_tcp_bind")
+  checkError(uv_listen(server, backlog, callback), "uv_tcp_listen")
+  uv_run(loop, UV_RUN_DEFAULT)
+
+  println(s"listening on $address:$port")
 
   val HTTP_request: Array[Byte] = "GET /birds/asdf HTTP/1.1\r\nHost: zxcv.com\r\n\r\n".getBytes
 
   process(HTTP_request)
 
   ////////////
+
+  def checkError(v: Int, label: String): Unit =
+    if v != 0 then
+      val error = fromCString(uv_err_name(v))
+      val message = fromCString(uv_strerror(v))
+
+      sys.error(s"$label returned $v: $error: $message")
 
   def process(httpreq: Array[Byte]): Unit =
     RequestParser run httpreq
---
+++ src/main/scala/io/github/edadma/spritz/libuvConstants.scala
@@ -0,0 +1,26 @@
+package io.github.edadma.spritz
+
+object libuvConstants:
+  // uv_run_mode
+  val UV_RUN_DEFAULT = 0
+  val UV_RUN_ONCE = 1
+  val UV_RUN_NOWAIT = 2
+
+  // UV_HANDLE_T
+  val UV_PIPE_T = 7 // Pipes
+  val UV_POLL_T = 8 // Polling external sockets
+  val UV_PREPARE_T = 9 // Runs every loop iteration
+  val UV_PROCESS_T = 10 // Subprocess
+  val UV_TCP_T = 12 // TCP sockets
+  val UV_TIMER_T = 13 // Timer
+  val UV_TTY_T = 14 // Terminal emulator
+  val UV_UDP_T = 15 // UDP sockets
+
+  // UV_REQ_T
+  val UV_WRITE_REQ_T = 3
+  val UV_SHUTDOWN_REQ_T = 4
+
+  val UV_READABLE = 1
+  val UV_WRITABLE = 2
+  val UV_DISCONNECT = 4
+  val UV_PRIORITIZED = 8
